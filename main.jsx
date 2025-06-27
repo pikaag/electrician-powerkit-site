23f68c17e753369b2ac9e032e3d7e4de78857858
pikaag <pikaaagl@gmail.com>
2025-06-27 13:32:59 +0200

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import PowerKitSite from './PowerKitSite'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PowerKitSite />
+  </React.StrictMode>
+)
